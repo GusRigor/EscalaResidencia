170aac7cbd7a71f38a570d6142166786f08a50d6
GusRigor <gusrigor@icloud.com>
2026-01-20 21:22:03 -0500

    Adiciona o Turno

--- escalas/models.py
+++ escalas/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from datetime import time
 
-# Create your models here.
+
+class Turno(models.Model):
+    class Codigo(models.TextChoices):
+        MANHA = 'MANHA', 'Manhã (6h)'
+        TARDE = 'TARDE', 'Tarde (6h)'
+        DIURNO = 'DIURNO', 'Manhã + Tarde (12h)'
+        NOITE = 'NOITE', 'Noite (12h)'
+
+    codigo = models.CharField(
+        max_length=10,
+        choices=Codigo.choices,
+        unique=True
+    )
+
+    nome = models.CharField(max_length=50)
+
+    hora_inicio = models.TimeField()
+    hora_fim = models.TimeField()
+
+    atravessa_dia = models.BooleanField(
+        default=False,
+        help_text='Indica se o turno termina no dia seguinte'
+    )
+
+    def __str__(self):
+        return self.nome
