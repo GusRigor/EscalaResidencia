b26b9d8cd65facc6793f05379d3162621aee7a93
GusRigor <gusrigor@icloud.com>
2026-01-20 21:12:16 -0500

    Adiciona o account

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = 'ADMIN', 'Administrador'
+        R1 = 'R1', 'Residente R1'
+        R2 = 'R2', 'Residente R2'
+        GUEST = 'GUEST', 'Convidado'
+
+    role = models.CharField(
+        max_length=10,
+        choices=Role.choices,
+        default=Role.GUEST
+    )
+
+    def is_resident(self):
+        return self.role in {self.Role.R1, self.Role.R2}
+
+    def is_admin(self):
+        return self.role == self.Role.ADMIN
